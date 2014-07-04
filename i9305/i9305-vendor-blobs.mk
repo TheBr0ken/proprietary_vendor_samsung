a76e2507b68218a619ccba92def97eebc0eadc3a
Darescu Ionut <ionut.darescu@gmail.com>
2014-07-04 05:34:38 -0400

    i9305: fix derp

--- i9305/i9305-vendor-blobs.mk
+++ i9305/i9305-vendor-blobs.mk
@@ -59,7 +59,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/usr/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/vendor-camera.exynos4.so:system/lib/hw/camera.vendor.exynos4.so
+    $(LOCAL_PATH)/proprietary/system/lib/hw/camera.vendor.exynos4.so:system/lib/hw/camera.vendor.exynos4.so
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/lib/hw/sensors.smdk4x12.so:system/lib/hw/sensors.smdk4x12.so
